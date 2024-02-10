a84c69f5789cc1a94dc102bf499bceddbfd1e888
hwcopeland <hwc2e@mtmail.mtsu.edu>
2024-02-10 00:03:28 -0500

    modified:   src/data.js

---
+++ src/data.js
@@ -0,0 +1,38 @@
+export const projects = [
+  {
+    title: "TUMDock",
+    subtitle: "The Unnamed Molecular Docking Project",
+    description:
+      "TUMDock is a fork of a project I started in my Medicinal Chemistry course. It is meant to be a simple, easy to use, and free molecular docking tool. It is currently in development, and is not yet ready for use.",
+    image: "/Docked-7JRN.gif",
+    link: "https://github.com/hwcopeland/TUMDock",
+  },
+  {
+    title: "HWCopeland.net",
+    subtitle: "Full Stack Web Portfolio",
+    description:
+      "A full stack web portfolio with React, Next.js, Tailwind-CSS and Sanity. This project was started as a place to digitalize and display my work. This site is self-hosted from homelab servers and is open source.",
+    image: "/web_vscode.png",
+    link: "https://github.com/hwcopeland/web",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "I have been extremely impressed with Hampton’s dedication and persistence.  It has become very obvious to me that even when Hampton does not know something, he puts in time and effort, and learns things very quickly. What is more impressive is that he has been able to learn on his own.  It is easy to work with Hampton as I never have to oversee every single step of Hampton’s research, but rather guide and direct him.  This tells me that Hampton is very much ready for research in a graduate program where the ability to work independently is crucial.  I am positive that Hampton is more than capable of performing research at any level (including graduate) because of his genuine interest in science, knowledge, skills, and desire to learn.",
+    image: "/Volkov_0.jpg",
+    name: "Dr. Anatoliy Volkov",
+    company: "Middle Tennessee State University",
+  },
+];
+
+/*export const skills = [
+  "JavaScript",
+  "React",
+  "Next.js",
+  "Node",
+  "GraphQL",
+  "Material UI",
+];
+*/
